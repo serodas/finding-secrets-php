7ff471bd732615a0e8fef7f66e8bbeaaccc30c4b
Samir Rodas <serodas9710@gmail.com>
2024-05-04 16:09:10 -0500

    Add migration and seeder in user microservice

---
+++ microservices/user/app/database/seeders/UsersSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->delete();
+        DB::table('users')->insert([
+            [
+                'email'     => 'serodas@phpmicroservices.com',
+                'password'  =>  Hash::make('12345678'),
+                'api_token' => NULL,
+            ],
+        ]);
+    }
+}
