66375fef0070b301689a087bceb539aaff37f7c0
Samir Rodas <serodas9710@gmail.com>
2024-05-06 15:03:19 -0500

    Add closest secrets endpoint based on the provided latitude and longitude

--- microservices/location/app/app/Http/Controllers/LocationController.php
+++ microservices/location/app/app/Http/Controllers/LocationController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 
 class LocationController extends Controller
@@ -11,54 +14,17 @@ class LocationController extends Controller
 
     const DEFAULT_CACHE_TIME    = 1;
 
-    public static $cacheSecrets = [
-        [
-            'id' => 100,
-            'name' => 'amber',
-            'location' => [
-                'latitude' => 42.8805, 'longitude' => -8.54569,
-                'name' => 'Santiago de Compostela'
-            ]
-        ],
-        [
-            'id' => 100,
-            'name' => 'diamond',
-            'location' => [
-                'latitude' => 38.2622, 'longitude' => -0.70107,
-                'name' => 'Elche'
-            ]
-        ],
-        [
-            'id' => 100,
-            'name' => 'pearl',
-            'location' => [
-                'latitude' => 41.8919, 'longitude' => 12.5113,
-                'name' => 'Rome'
-            ]
-        ],
-        [
-            'id' => 100,
-            'name' => 'ruby',
-            'location' => [
-                'latitude' => 53.4106, 'longitude' => -2.9779,
-                'name' => 'Liverpool'
-            ]
-        ],
-        [
-            'id' => 100,
-            'name' => 'sapphire',
-            'location' => [
-                'latitude' => 50.08804, 'longitude' => 14.42076,
-                'name' => 'Prague'
-            ]
-        ],
-    ];
-
-
     public static array $conversionRates = [
         'km' => 1.853159616,
         'mile' => 1.1515
     ];
+
+    public function findClosestSecrets(Request $request): JsonResponse
+    {
+        $originPoint = $request->only(['latitude', 'longitude']);
+        $secrets = $this->getClosestSecrets($originPoint);
+        return response()->json($secrets);
+    }
 
     public function getDistance($pointA, $pointB, $unit = 'km'): float
     {
@@ -67,17 +33,22 @@ class LocationController extends Controller
 
     public function getClosestSecrets($originPoint): array
     {
+        $client  = new Client(['verify' => false]);
+        $remoteCall = $client->get('http://microservice_secret_nginx/api/v1/secrets');
+        $data = json_decode($remoteCall->getBody(), true);
+        $secrets = $data['data'];
+
         $cacheKey = 'L' . $originPoint['latitude'] . $originPoint['longitude'];
         $closestSecrets = Cache::remember(
             $cacheKey,
             self::DEFAULT_CACHE_TIME,
-            function () use ($originPoint) {
+            function () use ($originPoint, $secrets) {
                 $calculatedClosestSecrets = [];
                 $distances = array_map(
                     function ($item) use ($originPoint) {
                         return $this->getDistance($item['location'], $originPoint);
                     },
-                    self::$cacheSecrets
+                    $secrets
                 );
                 asort($distances);
                 $distances = array_slice(
@@ -87,7 +58,7 @@ class LocationController extends Controller
                     true
                 );
                 foreach ($distances as $key => $distance) {
-                    $calculatedClosestSecrets[] = self::$cacheSecrets[$key];
+                    $calculatedClosestSecrets[] = $secrets[$key];
                 }
                 return $calculatedClosestSecrets;
             }
